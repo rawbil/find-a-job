f9430453bb447e47d712207ddc0b6d58592bc004
hackTivist <bildadsimiyu6@gmail.com>
2025-06-21 11:48:46 +0300

    feat: Add profile schema fields

--- backend/models/profile.model.ts
+++ backend/models/profile.model.ts
@@ -1,13 +1,45 @@
-import mongoose, { Schema, Document, Model} from 'mongoose';
+import mongoose, { Schema, Document, Model } from "mongoose";
 
-export interface IProfile extends Document {
-
+export interface IProfile {
+  profileImage: {
+    public_id: string;
+    url: string;
+  };
+  name: string;
+  location: string;
+  skills: string[];
+  phoneNumber: string;
+  email: string;
 }
 
 const profileSchema = new Schema<IProfile>({
-
+  profileImage: {
+    public_id: { type: String, required: true },
+    url: { type: String, required: true },
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  skills: {
+    type: [String],
+    required: true,
+  },
 });
 
-const profileModel: Model<IProfile> = mongoose.model("Profile", profileSchema );
+const profileModel: Model<IProfile> = mongoose.model("Profile", profileSchema);
 
 export default profileModel;
