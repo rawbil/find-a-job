90a8c19d5ae9d254b9316122fe5a0c3b6f83a09a
hackTivist <bildadsimiyu6@gmail.com>
2025-06-21 21:58:10 +0300

    feat: Add update profile, get single profile and get all profiles controllers and routes

--- backend/controllers/profile.controller.ts
+++ backend/controllers/profile.controller.ts
@@ -1,4 +1,4 @@
-import ProfileModel from "../models/profile.model";
+import ProfileModel, { IProfile } from "../models/profile.model";
 import { Request, Response, NextFunction } from "express";
 import ErrorHandler from "../utils/ErrorHandler";
 import cloudinary from "cloudinary";
@@ -28,6 +28,7 @@ export const CreateUserProfile = async (
   try {
     const data = req.body as ICreateProfile;
     const userId = req.user?._id as string;
+
     //ensure required fields are provided
     if (
       !data.name ||
@@ -83,7 +84,7 @@ export const CreateUserProfile = async (
       skills: data.skills,
       phoneNumber: data.phoneNumber,
       profileImage: data.profileImage,
-      location: data.location
+      location: data.location,
     });
     res.status(201).json({
       success: true,
@@ -96,12 +97,111 @@ export const CreateUserProfile = async (
 };
 
 //!UPDATE USER PROFILE
+interface IUpdateProfile {
+  profileImage?:
+    | string
+    | {
+        public_id: string;
+        url: string;
+      };
+  name?: string;
+  location?: string;
+  skills?: string[];
+  phoneNumber?: string;
+  email?: string;
+}
+
 export const UpdateUserProfile = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
   try {
+    const data = req.body as IUpdateProfile;
+    const userId = req.user?._id as string;
+
+    //check if updated email exists
+    if (data.email) {
+      const isProfileEmailExists = await ProfileModel.findOne({
+        email: data.email as string,
+      });
+      if (isProfileEmailExists) {
+        return next(
+          new ErrorHandler(
+            "Another profile with this email exists, can you provide a different one?",
+            409
+          )
+        );
+      }
+    }
+
+    //get profile from DB
+    const savedProfile = (await ProfileModel.findOne({
+      user: userId,
+    })) as IProfile;
+    if (!savedProfile) {
+      return next(new ErrorHandler("Profile not found", 404));
+    }
+
+    //update cloudinary image if changed
+    if (data.profileImage) {
+      if (savedProfile.profileImage?.public_id) {
+        try {
+          //delete existing image from cloudinary before uploading the new one
+          await cloudinary.v2.uploader.destroy(
+            savedProfile.profileImage?.public_id
+          );
+
+          //add the new image to cloudinary
+          const myCloud = await cloudinary.v2.uploader.upload(
+            data.profileImage as string,
+            {
+              folder: "Kazi-Profiles",
+            }
+          );
+          data.profileImage = {
+            public_id: myCloud.public_id,
+            url: myCloud.secure_url,
+          };
+        } catch (error: any) {
+          return next(new ErrorHandler(error.message, 500));
+        }
+      } else {
+        try {
+          const myCloud = await cloudinary.v2.uploader.upload(
+            data.profileImage as string,
+            {
+              folder: "Kazi-Profiles",
+            }
+          );
+          data.profileImage = {
+            public_id: myCloud.public_id,
+            url: myCloud.secure_url,
+          };
+        } catch (error: any) {
+          return next(new ErrorHandler(error.message, 500));
+        }
+      }
+    }
+
+    const updatedProfile = await ProfileModel.findByIdAndUpdate(
+      savedProfile._id,
+      {
+        $set: data,
+      },
+      { new: true }
+    );
+
+    /*
+    -$set is used to update only those fields that have changed, instead of the whole body.
+    -If not used and you just say `await ProfileModel.findByIdAndUpdate(userId, data, {new: true})`, it will update everything, and the fields not defined will be set to null
+    -Another approach is to manually declare the fields that have changed */
+
+    res.status(200).json({
+      success: true,
+      updatedProfile,
+      message: "Profile updated successfully",
+    });
   } catch (error: any) {
     return next(new ErrorHandler(error.message, 500));
   }
@@ -114,18 +214,31 @@ export const GetUserProfile = async (
   next: NextFunction
 ) => {
   try {
+    const userId = req.user?._id as string;
+    const profile = await ProfileModel.findOne({ user: userId });
+    if (!profile) {
+      return next(new ErrorHandler("Profile not found", 404));
+    }
+
+    res.status(200).json({ success: true, profile });
   } catch (error: any) {
     return next(new ErrorHandler(error.message, 500));
   }
 };
 
 //!GET ALL PROFILES
-export const GetAllUsersProfile = async (
+export const GetAllUsersProfiles = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
   try {
+    const profiles = await ProfileModel.find();
+    if (profiles.length === 0) {
+      return next(new ErrorHandler("No Profile to display", 404));
+    }
+
+    res.status(200).json({ success: true, profiles });
   } catch (error: any) {
     return next(new ErrorHandler(error.message, 500));
   }
--- backend/routes/profile.route.ts
+++ backend/routes/profile.route.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 import AuthMiddleware from '../middleware/auth.middleware';
-import { CreateUserProfile } from '../controllers/profile.controller';
+import { CreateUserProfile, GetAllUsersProfiles, GetUserProfile, UpdateUserProfile } from '../controllers/profile.controller';
 const router = express.Router();
 
 //api/profile/create
 router.post('/create', AuthMiddleware, CreateUserProfile);
+//api/profile/update
+router.patch('/update', AuthMiddleware, UpdateUserProfile);
+//api/profile/get-profile
+router.get('/get-profile', AuthMiddleware, GetUserProfile);
+//api/profiles/all
+router.get('/all', AuthMiddleware, GetAllUsersProfiles);
 
 
 export default router;
